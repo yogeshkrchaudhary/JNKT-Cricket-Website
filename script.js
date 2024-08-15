904019d4575f98b2794e5de29826a20f660e8c62
yogeshkrchaudhary <112309487+yogeshkrchaudhary@users.noreply.github.com>
2024-08-15 17:33:03 +0530

    all js update

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+//toggle icon 
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+};
+
+// swiper section
+
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1, // Default to 1 slide per view on mobile
+  slidesPerView: 4,
   spaceBetween: 30,
+  slidesPerGroup: 4,
   loop: true,
   grabCursor: true,
   loopFillGroupWithBlank: true,
@@ -12,12 +24,44 @@ var swiper = new Swiper(".mySwiper", {
      nextEl: ".swiper-button-next",
      prevEl: ".swiper-button-prev",
    },
-  breakpoints: {
-    768: {
-      slidesPerView: 2, // For tablets
-    },
-    1024: {
-      slidesPerView: 4, // For desktops
-    }
-  }
 });
+
+//scroll section
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height) {
+      // active navbar links
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+      });
+    };
+  });
+  // stickey header
+  let header = document.querySelector('.header');
+
+  header.classList.toggle('sticky', window.scrollY > 100);
+
+  //toggle icon remove when scroll
+  menuIcon.classList.remove('bx-x');
+  navbar.classList.remove('active');
+
+};
+
+// dark light mode 
+let darkModeIcon = document.querySelector('#darkMode-icon');
+
+darkModeIcon.onclick = () => {
+  darkModeIcon.classList.toggle('bx-sun');
+  document.body.classList.toggle('dark-mode');
+};
+
+
